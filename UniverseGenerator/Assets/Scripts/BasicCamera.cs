d9e6a0d06eb622d8dbee38b1bd0a315f2212a042
Ritalin4Kidz <callum@hands.net.au>
2018-09-13 18:38:44 +1000

    Inital Commit

---
+++ UniverseGenerator/Assets/Scripts/BasicCamera.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicCamera : MonoBehaviour
+{
+
+    /*
+    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
+    Converted to C# 27-02-13 - no credit wanted.
+    Simple flycam I made, since I couldn't find any others made public.  
+    Made simple to use (drag and drop, done) for regular keyboard layout  
+    wasd : basic movement
+    shift : Makes camera accelerate
+    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
+
+
+    float mainSpeed = 100.0f; //regular speed
+    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
+    float maxShift = 1000.0f; //Maximum speed when holdin gshift
+    float camSens = 0.25f; //How sensitive it with mouse
+    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+    private float totalRun = 1.0f;
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+    public GameObject Sun;
+
+    void Update()
+    {
+        restUpdate();
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+
+        transform.Translate(move, Space.World);
+    }
+    //Mouse  camera angle done.  
+
+    //Keyboard commands
+    public void restUpdate()
+    {
+    float f = 0.0f;
+        Vector3 p = GetBaseInput();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            totalRun += Time.deltaTime;
+            p = p * totalRun * shiftAdd;
+            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+        }
+        else
+        {
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+        }
+
+        p = p * Time.deltaTime;
+        Vector3 newPosition = transform.position;
+        if (Input.GetKey(KeyCode.Space))
+        { //If player wants to move on X and Z axis only
+            transform.Translate(p);
+            newPosition.x = transform.position.x;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+        else
+        {
+            transform.Translate(p);
+        }
+
+    }
+
+    private Vector3 GetBaseInput()
+    { //returns the basic values, if it's 0 than it's not active.
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //transform.LookAt(Sun.transform);
+            transform.RotateAround(Sun.transform.position, Vector3.up,  dragSpeed);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            //transform.LookAt(Sun.transform);
+            transform.RotateAround(Sun.transform.position, -Vector3.up, dragSpeed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+        return p_Velocity;
+    }
+}
---
+++ UniverseGenerator/Assets/Scripts/GalaxyGeneration.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalaxyGeneration : MonoBehaviour {
+    public GameObject planetRef;
+
+    int planetSize;
+    float planetSpawn = 0;
+    GameObject[] planets;
+	// Use this for initialization
+	void Start () {
+        int planetSize = Random.Range(6, 15);
+        planetSpawn += this.transform.localScale.x;
+        planets = new GameObject[planetSize];
+		for (int i = 0; i < planetSize; i++ )
+        {
+            GameObject planet = Instantiate(planetRef);
+            planet.GetComponent<PlanetScript>().init();
+            planetSpawn += planet.GetComponent<PlanetScript>().size;
+            planet.transform.position = new Vector3(planetSpawn, 0, 0);
+            planetSpawn += planet.GetComponent<PlanetScript>().size;
+            planet.transform.parent = this.transform;
+            this.transform.eulerAngles = new Vector3(0, Random.Range(0, 360), 0);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
